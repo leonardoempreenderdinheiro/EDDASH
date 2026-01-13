9c740927e10865702429c2045f89e172c600f0ed
HirataCompany <leonardoempreenderdinheiro@gmail.com>
2026-01-13 17:06:04 -0300

    Versão 4

--- pixel-perfect-copy-main/src/App.tsx
+++ pixel-perfect-copy-main/src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { HashRouter, Routes, Route, useLocation } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { FilterProvider } from "@/contexts/FilterContext";
 import { ThemeProvider } from "@/contexts/ThemeContext";
@@ -148,9 +148,9 @@ const App = () => (
           <TooltipProvider>
             <Toaster />
             <Sonner />
-            <BrowserRouter>
+            <HashRouter>
               <AnimatedRoutes />
-            </BrowserRouter>
+            </HashRouter>
           </TooltipProvider>
         </FilterProvider>
       </AuthProvider>
